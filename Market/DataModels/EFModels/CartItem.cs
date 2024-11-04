4630baa1e3d4c34f51d847c67fe5ba9e3aa6a904
Javier18121998 <javier.marin.garcia.linkedin01@hotmail.com>
2024-11-03 20:39:48 -0600

    The bug of addItems into a own Customer Cart is fixed now.

--- Market/DataModels/EFModels/CartItem.cs
+++ Market/DataModels/EFModels/CartItem.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Market.DataModels.EFModels
@@ -13,8 +14,11 @@ namespace Market.DataModels.EFModels
         public int ProductId { get; set; }
         public int Quantity { get; set; }
         public string? Size { get; set; }
+        [JsonIgnore]
         public decimal Price { get; set; }
+        [JsonIgnore]
         public Cart Cart { get; set; }
+        [JsonIgnore]
         public Product Product { get; set; }
     }
 }
